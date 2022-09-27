0e2d0e41d1e3283f01488ce4131715477f8dc775
prajwal971 <prajwalkumar971@gmail.com>
2022-09-27 14:24:57 +0400

    Successful RUN on Sql Query get and post initialised with JSON.

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,38 @@ const db = mysql.createConnection({
     user: "root",
     password: "Prajwal971@",
     database: "test",
-  });
-  
-  app.get("/", (req, res) => {
+});
+
+app.use(express.json())//allows to send any json file to client
+
+app.get("/", (req, res) => {
     res.json("hello");
-  });
-  
-  app.get("/books", (req, res) => {
+});
+
+app.get("/books", (req, res) => {
     const q = "SELECT * FROM books";
     db.query(q, (err, data) => {
-      if (err) {
-        console.log(err);
-        return res.json(err);
-      }
-      return res.json(data);
+        if (err) {
+            console.log(err);
+            return res.json(err);
+        }
+        return res.json(data);
     });
-  });
+});
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books(`title`, `desc`,`cover`) VALUE (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ]
+
+    db.query(q,[values],(err,data)=>{
+        if (err) console.log(err);
+        return res.json("Book has been created Successfully");
+    })
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend")

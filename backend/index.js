fd3ef1c38303774bc1cf98c4362514bd44fedd16
prajwal971 <prajwalkumar971@gmail.com>
2022-09-27 14:04:05 +0400

    Successful Auth and RUN on Sql Query.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import mysql from "mysql"
 
-const app = express()
+const app = express();
 
 const db = mysql.createConnection({
-    host: "Localhost",
+    host: "localhost",
     user: "root",
     password: "Prajwal971@",
-    database: "test"
-})
-
-app.get("/", (req, res) => {
-    res.json("Hello this is the backend");
-})
-
-
+    database: "test",
+  });
+  
+  app.get("/", (req, res) => {
+    res.json("hello");
+  });
+  
+  app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+  });
 
 app.listen(8800, () => {
     console.log("Connected to backend")
